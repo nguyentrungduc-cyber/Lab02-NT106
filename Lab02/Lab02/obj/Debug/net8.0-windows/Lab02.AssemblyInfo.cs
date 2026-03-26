9e0061e5ead0336f46efb9a9d835f14f3e672031
tachalorah <local>
2026-03-26 08:28:56 +0700

    Thêm .gitignore

--- Lab02/Lab02/obj/Debug/net8.0-windows/Lab02.AssemblyInfo.cs
+++ Lab02/Lab02/obj/Debug/net8.0-windows/Lab02.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab02")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e7e4fb69d822e21b578ef65676ab44c2735e7d6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab02")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab02")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
